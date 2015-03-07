0b6681691311784118bc8b1b0d57800c44bd350d
stanford <stanford@stanford-VirtualBox.(none)>
2015-03-07 15:35:47 -0500

    modified data

--- preprocessing/export_bag_data.py
+++ preprocessing/export_bag_data.py
@@ -9,14 +9,13 @@ from copy import copy
 parser = argparse.ArgumentParser(description='Process some integers.')
 parser.add_argument('bag_files', metavar='bags', type=str, nargs='+',
                    help='file names for bags')
-parser.add_argument('--topic', dest='image_topic', default='sibot/left/image_rect', help='image topic to look at')
 parser.add_argument('--out', dest='out_path', default='sibot/left/image_rect', help='output path')
 
 args = parser.parse_args()
 
 # get bags from arguments
 bagfiles = args.bag_files
-image_topic = args.image_topic
+image_topics = ['sibot/left/image_rect', 'sibot/right/image_rect']
 base_image_path = args.out_path
 out_filename = 'out.txt'
 bin_count = 20
@@ -38,34 +37,38 @@ def classify_joy(right_lr_axis, bins):
     #        return i
     #return bins - 1
 
-out_file = open(join(base_image_path, out_filename), 'w')
 
 for bagfile in bagfiles:
-    # mkdir for bagfile
-    base_path = splitext(basename(bagfile))[0]
-    image_dir = join(base_image_path, base_path)
-    if not exists(image_dir):
-        makedirs(image_dir)
+    base_bag_name = splitext(basename(bagfile))[0]
+    topic_to_out_file = {}
+    topic_to_image_dir = {}
+    for topic in image_topics:
+        dir_ext = "-".join(topic.split('/')[1:])
+        dir_name = "{0}-{1}".format(base_bag_name, dir_ext)
+        image_dir = join(base_image_path, dir_name)
+        if not exists(image_dir):
+            makedirs(image_dir)
+        topic_to_image_dir[topic] = image_dir
+        topic_to_out_file[topic] = open(join(image_dir, out_filename), 'w')
     bag = rosbag.Bag(bagfile)
     last_joy_msg = None
     last_joy_time = None
     i = 0
     prev_t = None
     for topic, msg, t in bag.read_messages():
-        if prev_t is not None:
-            print t - prev_t
         if topic == 'joy':
             last_joy_msg = copy(msg)
             last_joy_time = copy(t)
-        elif topic == image_topic:
+        elif topic in topic_to_image_dir:
             image_name = "frame{:03}.jpg".format(i)
             image_path = join(image_dir, image_name)
             cv_image = bridge.imgmsg_to_cv2(msg)
             cv_image = cv2.resize(cv_image, out_image_size)
             correct_class = classify_joy(last_joy_msg.axes[3], bin_count)
-            if cv2.imwrite(image_path, cv_image):
-                line = "{0} {1}\n".format(join(base_path, image_name), correct_class)
+            if cv2.imwrite(join(topic_to_image_dir[topic], image_name), cv_image):
+                line = "{0} {1}\n".format(image_name, correct_class)
+                topic_to_out_file[topic].write(line)
                 i += 1
-            
-            out_file.write(line)
+    for topic in topic_to_out_file:
+        topic_to_out_file[topic].close()
  
